60dbac77d87abb80a49c2e59d8019fecae3b441d
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 03:53:42 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -34,7 +34,11 @@ contacts = {
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-  binding.pry
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      binding.pry
+    end
+  end
 end
 
 
