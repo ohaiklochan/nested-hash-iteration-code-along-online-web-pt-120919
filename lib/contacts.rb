1e1de21dfe372c834c4ce2435ecf5d096680e5ef
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 03:52:35 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -10,10 +10,31 @@ require 'pry'
   #   "Freddy Mercury" => {
   #     name: "Freddy",
   #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  #     favorite_ice_cream_flavors: ["cookie dough", "mint chip"]
   #   }
   # }
   
-def remove_strawberry(contacts)
+  
+  
+contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we", 
+    favorite_ice_cream_flavors: ["chocolate", "vanilla", "mint chip"],
+        knows: nil
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["cookie dough", "mint chip"]
+  }
+}
+ 
 
+
+def remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
+  binding.pry
 end
+
+
