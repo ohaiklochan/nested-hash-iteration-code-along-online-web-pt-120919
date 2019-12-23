a8ce47006dfaef602079be4ec35a3b829dfd3882
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 03:57:14 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,13 +16,13 @@ def people
   }
 end 
 
-
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-  if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_ice_cream_flavors
-        binding.pry
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
       end
     end
   end
